a3ea72a5d17e32819218f997a07b1d40f992a378
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-11-02 16:37:23 +0700

    Mengecek sedikit perubahan

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-$('#search-button')
+$('#search-button').click(function () {
+    fetch(`https://api.jikan.moe/v3/search/anime?q=${$("#search").val()}`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText)
+        })
+        .then(response => {
+            console.log(response)
+        })
+});

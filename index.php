06717a73de5b7e92c2a62c6ac0a8db069d98206a
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-11-03 11:45:44 +0700

    Sip modalnya udah selesai

--- index.php
+++ index.php
@@ -54,18 +54,17 @@
 
     <!-- Modal -->
     <div class="modal fade" id="animeModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <h5 class="modal-title" id="exampleModalLabel">Anime Details</h5>
+                    <button type="button" class="close" id="tanda-silang" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body"></div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal" id="animeModal-close">Close</button>
                 </div>
             </div>
         </div>
